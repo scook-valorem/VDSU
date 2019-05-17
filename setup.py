82b3fc6aeb38e070fd9074d5668d085c72cd3039
Spencer Cook <scook@valorem.com>
2019-05-17 16:32:53 -0500

    Adding setup.py
    Signed-off-by: Spencer Cook <scook@valorem.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='VDSU',
+    version='0.0.1',
+    packages=['VDSU'],
+    url='https://github.com/scook-valorem/VDSU.git',
+    license='',
+    author='scook',
+    author_email='scook@valorem.com',
+    description='Valorem Data Science Utilities'
+)
